b3431d605695ea235a75d8d941d25c8ac571b9b0
Diego Ocampo <diegoocampo8@gmail.com>
2015-02-25 14:03:36 +0100

    Ready for production

--- launcher_ch/settings.py
+++ launcher_ch/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chattybetalaunch',       # Or path to database file if using sqlite3.
+        'NAME': 'chattylaunch',       # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'chlaunchuser',
         'PASSWORD': 'ch19ke9s7udcmrpalw34cx1',
@@ -170,7 +170,7 @@ LOGGING = {
 
 
 # Parse database configuration from $DATABASE_URL
-DATABASES = {'default': dj_database_url.config(default='postgres://chlaunchuser:ch19ke9s7udcmrpalw34cx1@localhost/chattybetalaunch')}
+DATABASES = {'default': dj_database_url.config(default='postgres://chlaunchuser:ch19ke9s7udcmrpalw34cx1@localhost/chattylaunch')}
 # DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
